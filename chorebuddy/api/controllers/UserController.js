53d99f8c219f3a7decc05e06e744b4850b734ee8
Martin Kubat <martinkubat@icloud.com>
2015-10-10 14:14:52 +0100

    Make controllers

--- chorebuddy/api/controllers/UserController.js
+++ chorebuddy/api/controllers/UserController.js
@@ -6,9 +6,24 @@
  */
 
 module.exports = {
+	create: (req, res) => {
+		User.create({
+			name: req.param('name'),
+			house: req.param('house'),
+			manager: req.param('manager'),
+			phoneNumber: req.param('phoneNumber'),
+		})
+		.then(user => {
+			res.json({ data: user });
+		})
+		.catch(err => {
+			res.json({ error: err});
+		});
+	},
+
 	addPreferedDays: (req, res) => {
 
 	},
-	
+
 
 };
